3d7ea9ee5265c83c8e7b5e3959ae9a0403a1c74a
jamesnll <langillejames11@gmail.com>
2024-03-07 13:10:57 -0800

    Compiled and built with Check, yippeee

---
+++ build/CMakeFiles/main.dir/src/math_operations.c.o.d
@@ -0,0 +1,3 @@
+CMakeFiles/main.dir/src/math_operations.c.o: \
+  /home/jamesnl/CLionProjects/COMP-4985-Protocol-Tester/src/math_operations.c \
+  /home/jamesnl/CLionProjects/COMP-4985-Protocol-Tester/src/../include/math_operations.h
