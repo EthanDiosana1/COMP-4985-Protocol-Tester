58aacc4eb40bfc38832d28e382a26e5e985eb33b
jamesnll <langillejames11@gmail.com>
2024-03-19 22:02:10 -0700

    Added mock server files

---
+++ src/protocolMockServer.c
@@ -0,0 +1,184 @@
+#include "../include/protocolMockServer.h"
+#include <arpa/inet.h>
+#include <errno.h>
+#include <inttypes.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+#define BASE_TEN 10
+
+static in_port_t parse_in_port_t(void);
+static void      convert_address(const char *address, struct sockaddr_storage *addr);
+static int       socket_create(int domain, int type, int protocol);
+static void      socket_setsockopt(int sockfd);
+static void      socket_bind(int sockfd, struct sockaddr_storage *addr, in_port_t port);
+static void      start_listening(int sockfd);
+
+int create_server(void)
+{
+    const char             *address = "127.0.0.1";
+    in_port_t               port;
+    int                     sockfd;
+    struct sockaddr_storage addr;
+
+    port = parse_in_port_t();
+    convert_address(address, &addr);
+    sockfd = socket_create(addr.ss_family, SOCK_STREAM, 0);
+    socket_setsockopt(sockfd);
+    socket_bind(sockfd, &addr, port);
+    start_listening(sockfd);
+
+    return sockfd;
+}
+
+in_port_t parse_in_port_t(void)
+{
+    char       *endptr;
+    const char *str = "8888";
+    uintmax_t   parsed_value;
+
+    parsed_value = strtoumax(str, &endptr, BASE_TEN);
+
+    return (in_port_t)parsed_value;
+}
+
+static void convert_address(const char *address, struct sockaddr_storage *addr)
+{
+    memset(addr, 0, sizeof(*addr));
+
+    if(inet_pton(AF_INET, address, &(((struct sockaddr_in *)addr)->sin_addr)) == 1)
+    {
+        addr->ss_family = AF_INET;
+    }
+    else if(inet_pton(AF_INET6, address, &(((struct sockaddr_in6 *)addr)->sin6_addr)) == 1)
+    {
+        addr->ss_family = AF_INET6;
+    }
+    else
+    {
+        fprintf(stderr, "%s is not an IPv4 or an IPv6 address\n", address);
+        exit(EXIT_FAILURE);
+    }
+}
+
+static int socket_create(int domain, int type, int protocol)
+{
+    int sockfd;
+
+    sockfd = socket(domain, type, protocol);
+
+    if(sockfd == -1)
+    {
+        perror("Socket creation failed");
+        exit(EXIT_FAILURE);
+    }
+
+    return sockfd;
+}
+
+static void socket_setsockopt(int sockfd)
+{
+    int enable;
+
+    enable = 1;
+    if(setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(int)) == -1)
+    {
+        perror("setsockopt failed");
+        exit(EXIT_FAILURE);
+    }
+}
+
+static void socket_bind(int sockfd, struct sockaddr_storage *addr, in_port_t port)
+{
+    char      addr_str[INET6_ADDRSTRLEN];
+    socklen_t addr_len;
+    void     *vaddr;
+    in_port_t net_port;
+
+    net_port = htons(port);
+
+    if(addr->ss_family == AF_INET)
+    {
+        struct sockaddr_in *ipv4_addr;
+
+        ipv4_addr           = (struct sockaddr_in *)addr;
+        addr_len            = sizeof(*ipv4_addr);
+        ipv4_addr->sin_port = net_port;
+        vaddr               = (void *)&(((struct sockaddr_in *)addr)->sin_addr);
+    }
+    else if(addr->ss_family == AF_INET6)
+    {
+        struct sockaddr_in6 *ipv6_addr;
+
+        ipv6_addr            = (struct sockaddr_in6 *)addr;
+        addr_len             = sizeof(*ipv6_addr);
+        ipv6_addr->sin6_port = net_port;
+        vaddr                = (void *)&(((struct sockaddr_in6 *)addr)->sin6_addr);
+    }
+    else
+    {
+        fprintf(stderr, "Internal error: addr->ss_family must be AF_INET or AF_INET6, was: %d\n", addr->ss_family);
+        exit(EXIT_FAILURE);
+    }
+
+    if(inet_ntop(addr->ss_family, vaddr, addr_str, sizeof(addr_str)) == NULL)
+    {
+        perror("inet_ntop");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Binding to: %s:%u\n", addr_str, port);
+
+    if(bind(sockfd, (struct sockaddr *)addr, addr_len) == -1)
+    {
+        perror("Binding failed");
+        fprintf(stderr, "Error code: %d\n", errno);
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Bound to socket: %s:%u\n", addr_str, port);
+}
+
+static void start_listening(int sockfd)
+{
+    const int backlog = 5;
+
+    if(listen(sockfd, backlog) == -1)
+    {
+        perror("listen failed");
+        close(sockfd);
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Listening for incoming connections...\n");
+}
+
+int accept_connection(int server_fd, struct sockaddr_storage *client_addr, socklen_t *client_addr_len)
+{
+    int  client_fd;
+    char client_host[NI_MAXHOST];
+    char client_service[NI_MAXSERV];
+
+    errno     = 0;
+    client_fd = accept(server_fd, (struct sockaddr *)client_addr, client_addr_len);
+
+    if(client_fd == -1)
+    {
+        return -1;
+    }
+
+    if(getnameinfo((struct sockaddr *)client_addr, *client_addr_len, client_host, NI_MAXHOST, client_service, NI_MAXSERV, 0) == 0)
+    {
+        printf("Accepted a new connection from %s:%s\n", client_host, client_service);
+    }
+    else
+    {
+        printf("Unable to get client information\n");
+    }
+
+    return client_fd;
+}
