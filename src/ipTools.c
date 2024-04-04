c5767e32446700bc3999b41568df6ed823fdd049
Ethan Diosana <ethandiosana1@gmail.com>
2024-04-03 20:11:22 -0700

    Started on prototype functions for ipTools

---
+++ src/ipTools.c
@@ -0,0 +1,21 @@
+#include "ipTools.h"
+
+/**
+ * @brief Converts a string to a uint16_t.
+ * @param str The string to convert.
+ * @returns uint16_t
+ */
+uint16_t str_to_uint16_t(const char *str)
+{
+    return 0;
+}
+
+/**
+ * @brief Returns 1 if the ip is valid and 0 if not.
+ * @param ip The ip to check.
+ * @returns 1 if ip is valid, 0 if not.
+ */
+bool is_valid_ipv4(const char *ip)
+{
+    return false;
+}
